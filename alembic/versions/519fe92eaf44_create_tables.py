de29465485103d2bf7a2f60dc9b7c5166bc108cf
MakuninIII <imac.55505@gmail.com>
2024-10-31 10:42:21 +0800

    fix

--- alembic/versions/519fe92eaf44_create_tables.py
+++ alembic/versions/519fe92eaf44_create_tables.py
@@ -8,6 +8,8 @@ Create Date: 2024-10-02 23:38:11.776309
 from alembic import op
 import sqlalchemy as sa
 
+#alembic upgrade <unique_id_here>
+
 # revision identifiers, used by Alembic.
 revision = 'unique_id_here'
 down_revision = None
@@ -21,7 +23,9 @@ def upgrade():
         sa.Column('name', sa.String(255), nullable=False),
         sa.Column('country', sa.String(255), nullable=False),
         sa.Column('debut_year', sa.SmallInteger, nullable=False),
-        sa.CheckConstraint('debut_year >= 1860 AND debut_year <= EXTRACT(YEAR FROM CURRENT_DATE)', name='check_artist_debut_year')
+        sa.CheckConstraint('debut_year >= 1860 AND debut_year <= EXTRACT(YEAR FROM CURRENT_DATE)', name='check_artist_debut_year'),
+        sa.CheckConstraint('LENGTH(BTRIM(name)) > 0', name='check_name_not_empty_string'),
+        sa.CheckConstraint('BTRIM(name) = name', name='check_name_without_spaces'),
     )
 
     op.create_table(
@@ -31,14 +35,16 @@ def upgrade():
         sa.Column('release_year', sa.SmallInteger, nullable=False),
         sa.Column('duration', sa.SmallInteger, nullable=False),
         sa.Column('artist_id', sa.Integer, sa.ForeignKey('artist.id', onupdate="CASCADE", ondelete="CASCADE"), nullable=False),
-        sa.CheckConstraint('release_year >= 1860 release_year <= EXTRACT(YEAR FROM CURRENT_DATE)', name='check_song_release_year'),
-        sa.CheckConstraint('duration >= 0', name='check_song_duration')
+        sa.CheckConstraint('release_year >= 1860 AND release_year <= EXTRACT(YEAR FROM CURRENT_DATE)', name='check_song_release_year'),
+        sa.CheckConstraint('duration > 0', name='check_song_duration')
     )
 
     op.create_table(
         'genre',
         sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('name', sa.String(255), nullable=False)
+        sa.Column('name', sa.String(255), nullable=False, unique=True),
+        sa.CheckConstraint('BTRIM(name) = name', name='check_name_without_spaces'),
+        sa.CheckConstraint('LENGTH(BTRIM(name)) > 0', name='check_name_not_empty_string')
     )
 
     op.create_table(
--- crud_artist.py
+++ crud_artist.py
@@ -1,17 +1,53 @@
 import psycopg2
+import time
+from datetime import datetime
+from prettytable import PrettyTable
 
 def get_connection():
     conn = psycopg2.connect(
-        dbname="Music2",
+        dbname="Test",
         user="postgres",
         password=" ",
         host="localhost"
     )
     return conn
 
-# Create - добавляет нового артиста
-def ArtistCreate(name, country, debut_year):
-    conn = get_connection()
+# 1.Create - добавляет нового артиста
+def ArtistCreate(conn):
+    while True:
+        name = input("Enter artist name: ")
+        
+        if not name:
+            print("Error: name cannot be empty.")
+            continue
+        
+        cur = conn.cursor()
+        cur.execute("SELECT id FROM Artist WHERE name = %s", (name,))
+        if cur.fetchone() is not None:
+            print("Error: An artist with this name already exists. Please enter a unique name.")
+            cur.close()
+        else:
+            cur.close()
+            break
+
+    while True:
+        country = input("Enter artist country: ")
+        if not country.isalpha():
+            print("Error: country should only contain letters.")
+        else:
+            break
+
+    current_year = datetime.now().year
+    while True:
+        try:
+            debut_year = int(input("Enter debut year: "))
+            if debut_year < 1860 or debut_year > current_year:
+                print(f"Error: debut year must be between 1860 and {current_year}.")
+            else:
+                break
+        except ValueError:
+            print("Error: debut year must be a valid number.")
+
     cur = conn.cursor()
     cur.execute(
         "INSERT INTO Artist (name, country, debut_year) VALUES (%s, %s, %s) RETURNING id",
@@ -20,36 +56,102 @@ def ArtistCreate(name, country, debut_year):
     artist_id = cur.fetchone()[0]
     conn.commit()
     cur.close()
-    conn.close()
     print(f"Artist created with ID: {artist_id}")
+    time.sleep(1)
 
-# RetrieveAll - получает всех артистов
-def ArtistRetrieveAll():
-    conn = get_connection()
+# 2.RetrieveAll - получает всех артистов
+def ArtistRetrieveAll(conn):
     cur = conn.cursor()
     cur.execute("SELECT * FROM Artist")
     artists = cur.fetchall()
-    for artist in artists:
-        print(artist)
+    
+    if not artists:
+        print("No artists found.")
+    else:
+        table = PrettyTable()
+        table.field_names = ["id", "Name", "Country", "Debut Year"]
+        for artist in artists:
+            table.add_row(artist)
+        print(table)
     cur.close()
-    conn.close()
 
-# Retrieve - получает одного артиста по первичному ключу
-def ArtistRetrieve(artist_id):
-    conn = get_connection()
+# 3.Retrieve - получает одного артиста по первичному ключу
+def ArtistRetrieve(conn):
+    while True:
+        try:
+            artist_id = int(input("Enter artist ID: "))
+            break 
+        except ValueError:
+            print("Error: Artist ID must be an integer. Please try again.")
+
     cur = conn.cursor()
     cur.execute("SELECT * FROM Artist WHERE id = %s", (artist_id,))
     artist = cur.fetchone()
+
     if artist:
-        print(artist)
+        table = PrettyTable()
+        table.field_names = ["id", "Name", "Country", "Debut Year"]
+        table.add_row(artist)
+        print(table)
     else:
         print(f"Artist with ID {artist_id} not found.")
-    cur.close()
-    conn.close()
+        time.sleep(1)
 
-# Update - обновляет информацию о конкретном артисте по его ID
-def ArtistUpdate(artist_id, name, country, debut_year):
-    conn = get_connection()
+    cur.close()
+
+
+# 4.Update - обновляет информацию о конкретном артисте по его ID
+def ArtistUpdate(conn):
+    while True:
+        try:
+            artist_id = int(input("Enter artist ID: "))
+            break 
+        except ValueError:
+            print("Error: Artist ID must be an integer. Please try again.")
+
+    cur = conn.cursor()
+    cur.execute("SELECT name, country, debut_year FROM Artist WHERE id = %s", (artist_id,))
+    artist = cur.fetchone()
+
+    if artist is None:
+        print(f"Artist with ID {artist_id} not found.")
+        cur.close()
+        time.sleep(1)
+        return
+    
+    current_name, current_country, current_debut_year = artist
+
+    
+    name = input(f"Enter new artist name (or leave empty to keep current - '{current_name}'): ")
+    if not name:
+        name = current_name
+
+    while True:
+        country = input(f"Enter new artist country (or leave empty to keep current - '{current_country}'): ")
+        if not country:
+            country = current_country
+            break
+        elif not country.isalpha():
+            print("Error: country should only contain letters.")
+        else:
+            break
+
+    current_year = datetime.now().year
+
+    while True:
+        debut_year = input(f"Enter new debut year (or leave empty to keep current - '{current_debut_year}'): ")
+        if not debut_year:
+            debut_year = current_debut_year
+            break
+        try:
+            debut_year = int(debut_year)
+            if debut_year < 1860 or debut_year > current_year:
+                print(f"Error: debut year must be between 1860 and {current_year}.")
+            else:
+                break
+        except ValueError:
+            print("Error: debut year must be a valid number.")
+
     cur = conn.cursor()
     cur.execute(
         "UPDATE Artist SET name = %s, country = %s, debut_year = %s WHERE id = %s",
@@ -58,45 +160,87 @@ def ArtistUpdate(artist_id, name, country, debut_year):
     conn.commit()
     if cur.rowcount:
         print(f"Artist with ID {artist_id} updated successfully.")
+        cur.execute("SELECT name, country, debut_year FROM Artist WHERE id = %s", (artist_id,))
+        updated_artist = cur.fetchone()
+        table = PrettyTable()
+        table.field_names = ["Name", "Country", "Debut Year"]
+        table.add_row(updated_artist)
+        print(f"Updated artist data: \n{table}")
     else:
         print(f"Artist with ID {artist_id} not found.")
-    cur.close()
-    conn.close()
 
-# Delete - удаляет артиста по его ID
-def ArtistDelete(artist_id):
-    conn = get_connection()
+    cur.close() 
+
+
+# 5.Delete - удаляет артиста по его ID
+def ArtistDelete(conn):
+    while True:
+        try:
+            artist_id = int(input("Enter artist ID: "))
+            break 
+        except ValueError:
+            print("Error: Artist ID must be an integer. Please try again.")
+
     cur = conn.cursor()
+
     cur.execute("DELETE FROM Artist WHERE id = %s", (artist_id,))
     conn.commit()
-    if cur.rowcount:
+    if cur.rowcount > 0:
         print(f"Artist with ID {artist_id} deleted successfully.")
+        time.sleep(1)
     else:
         print(f"Artist with ID {artist_id} not found.")
+        time.sleep(1)
+    
     cur.close()
-    conn.close()
 
-# DeleteMany - удаляет несколько артистов по их ID
-def ArtistDeleteMany(artist_ids):
-    conn = get_connection()
+
+# 6.DeleteMany - удаляет несколько артистов по их ID
+def ArtistDeleteMany(conn):
+    while True:
+        artist_ids = input("Enter artist IDs separated by commas: ")
+        artist_ids = [x.strip() for x in artist_ids.split(",")]
+
+        if all(x.isdigit() for x in artist_ids):
+            artist_ids = [int(x) for x in artist_ids]
+            break
+        else:
+            print("Error: Please enter valid artist IDs separated by commas. Only numeric IDs are allowed.")
+
     cur = conn.cursor()
-    cur.execute("DELETE FROM Artist WHERE id IN %s", (tuple(artist_ids),))
-    conn.commit()
-    print(f"Artists with IDs {artist_ids} deleted successfully.")
-    cur.close()
-    conn.close()
+    cur.execute("SELECT id FROM Artist WHERE id IN %s", (tuple(artist_ids),))
+    existing_ids = [row[0] for row in cur.fetchall()]
 
-# лаб. 2 Search - поиск экземпляров сущности по указанным пользователем значениям атрибутов
-def ArtistSearch(name=None, country=None, debut_year=None, limit=5, offset=0):
-    conn = get_connection()
+    if not existing_ids:
+        print("No artists found with the provided IDs.")
+        cur.close()
+        time.sleep(1)
+        return
+
+    cur.execute("DELETE FROM Artist WHERE id IN %s", (tuple(existing_ids),))
+    conn.commit()
+
+    not_found_ids = set(artist_ids) - set(existing_ids)
+
+    if existing_ids:
+        print(f"Artists with IDs {set(existing_ids)} deleted successfully.")
+    if not_found_ids:
+        print(f"No artists found with IDs: {not_found_ids}")
+
+    cur.close()
+    time.sleep(1)
+
+
+# лаб. 2 // 7.Search - поиск экземпляров сущности по указанным пользователем значениям атрибутов
+def ArtistSearch(conn, name=None, country=None, debut_year=None, limit=5, offset=0):
     cur = conn.cursor()
 
     query = "SELECT * FROM Artist WHERE 1=1"
     params = []
 
     if name is not None:
-        query += " AND name = %s"
-        params.append(name)
+        query += " AND name LIKE %s"
+        params.append(f"%{name}%")
     if country is not None:
         query += " AND country = %s"
         params.append(country)
@@ -117,11 +261,18 @@ def ArtistSearch(name=None, country=None, debut_year=None, limit=5, offset=0):
         print("No artists found matching the criteria.")
 
     cur.close()
-    conn.close()
 
 
 # main
 if __name__ == "__main__":
+
+    conn = psycopg2.connect(
+        dbname="Test",
+        user="postgres",
+        password=" ",
+        host="localhost"
+    )
+        
     while True:
         print("\n1. Create Artist")
         print("2. Retrieve All Artists")
@@ -129,36 +280,23 @@ if __name__ == "__main__":
         print("4. Update Artist")
         print("5. Delete Artist by ID")
         print("6. Delete Many Artists")
-        print("7. Exit")
-        print("8. Search Artist by attributes")
+        print("7. Search Artist by attributes")
+        print("8. Exit")
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            name = input("Enter artist name: ")
-            country = input("Enter artist country: ")
-            debut_year = input("Enter debut year: ")
-            ArtistCreate(name, country, debut_year)
+            ArtistCreate(conn)
         elif choice == '2':
-            ArtistRetrieveAll()
+            ArtistRetrieveAll(conn)
         elif choice == '3':
-            artist_id = int(input("Enter artist ID: "))
-            ArtistRetrieve(artist_id)
+            ArtistRetrieve(conn)
         elif choice == '4':
-            artist_id = int(input("Enter artist ID: "))
-            name = input("Enter new artist name: ")
-            country = input("Enter new artist country: ")
-            debut_year = input("Enter new debut year: ")
-            ArtistUpdate(artist_id, name, country, debut_year)
+            ArtistUpdate(conn)
         elif choice == '5':
-            artist_id = int(input("Enter artist ID: "))
-            ArtistDelete(artist_id)
+            ArtistDelete(conn)
         elif choice == '6':
-            artist_ids = input("Enter artist IDs separated by commas: ")
-            artist_ids = [int(x) for x in artist_ids.split(",")]
-            ArtistDeleteMany(artist_ids)
+            ArtistDeleteMany(conn)
         elif choice == '7':
-            break
-        elif choice == '8':
             name = input("Enter name (or press Enter to skip): ") or None
             country = input("Enter artist's country (or press Enter to skip): ") or None
             debut_year = input("Enter debut year (or press Enter to skip): ") or None
@@ -168,9 +306,11 @@ if __name__ == "__main__":
             
             offset_count = input("Enter offset (default 0): ")
             offset_count = int(offset_count) if offset_count else 0
-            
-            ArtistSearch(name, country, debut_year, limit_count, offset_count)
+
+            ArtistSearch(conn, name, country, debut_year, limit_count, offset_count)
+        elif choice == '8':
+            break
         else:
             print("Invalid choice. Please try again.")
 
-
+    conn.close()
